97808d7c28c6596b75f2e03c073def056c397671
François Saura <franois.saura@gmail.com>
2022-04-20 22:21:52 +0200

    Ajout d'une classe Film

---
+++ ExoHttpAPI/Film.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoHttpAPI
+{
+    public class Film
+    {
+        public int id;
+        public string titre;
+        public string description;
+        public string synopsis;
+        public string image_url;
+        public bool user_liked;
+        public bool user_loved;
+        public bool moviedb_json;
+    }
+}
